e447322560113b64190b17544e2b88c6b9cdb1b0
Aoi <ht.so.ik0102@gmail.com>
2021-10-07 20:54:44 +0700

    fix(test): test1
    test123

---
+++ contracts/Token.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.8.4;
+
+contract Token {
+    string public name = "My Hardhat Token";
+    string public symbol = "MHT";
+
+    uint256 public totalSupply = 1000000;
+
+    address public owner;
+
+    mapping(address => uint256) balances;
+
+    constructor() {
+        balances[msg.sender] = totalSupply;
+        owner = msg.sender;
+    }
+}
