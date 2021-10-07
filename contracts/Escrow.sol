d42075493447fc15a84e215a0dd646d00beea240
Aoi <ht.so.ik0102@gmail.com>
2021-10-07 23:45:21 +0700

    feat(escrow.sol): add new some functions

--- contracts/Escrow.sol
+++ contracts/Escrow.sol
@@ -64,4 +64,70 @@ contract Escrow {
 
         require((2 * price) == msg.value, "Value has to be even");
     }
+
+    function close() public onlySeller inState(State.Sale) {
+        state = State.Closed;
+        seller.transfer(address(this).balance);
+
+        emit Closed(block.timestamp);
+    }
+
+    function confirmPurchase() public payable notSeller inState(State.Sale) condition(msg.value == (2 * price)) {
+        buyer = payable(msg.sender);
+        state = State.Locked;
+
+        emit ConfirmPurchase(block.timestamp, buyer);
+    }
+
+    function confirmReceived() public onlyBuyer inState(State.Locked) {
+        state = State.Release;
+
+        buyer.transfer(price);
+        emit ConfirmReceived(block.timestamp, buyer);
+    }
+
+    function refundBuyer() public onlySeller inState(State.Locked) {
+        state = State.Sale;
+        buyer = payable(0);
+
+        emit SellerRefundBuyer(block.timestamp);
+    }
+
+    function refundSeller() public onlySeller inState(State.Release) {
+        state = State.Complete;
+
+        seller.transfer(3 * price);
+
+        previousBuyers.push(buyer);
+
+        emit SellerRefunded(block.timestamp);
+    }
+
+    function restartContract() public payable onlySeller {
+        if (state == State.Closed || state == State.Complete) {
+            require((2 * price) == msg.value, "Valiue has to be equal to what started the contract.");
+
+            state = State.Sale;
+
+            buyer = payable(0);
+
+            emit Restarted(block.timestamp);
+        }
+    }
+
+    function listPreviousBuyers() public view returns (address[] memory) {
+        return previousBuyers;
+    }
+
+    function totalSales() public view returns (uint256 count) {
+        return previousBuyers.length;
+    }
+
+    function end() public onlySeller {
+        if (state == State.Closed || state == State.Complete) {
+            emit End(block.timestamp);
+
+            selfdestruct(seller);
+        }
+    }
 }
